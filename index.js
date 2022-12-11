22ac05149c6373bdabf2801a58a49aa63573a3ee
azizulhakims <it.azizulhakim@gmsil.com>
2022-12-11 11:20:13 +0600

    add server

---
+++ index.js
@@ -0,0 +1,39 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors')
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gyop3ad.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    // try{
+
+    // }
+
+
+}
+
+run().catch(err => console.log(err));
+
+
+app.get('/', (req, res) => {
+    res.send('server running');
+});
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server running PORT no ${port}`);
+})
+
