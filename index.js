21750130702eaaddbaa123bd6b930636f7853fee
azizulhakims <it.azizulhakim@gmsil.com>
2022-12-11 18:30:05 +0600

    add vercel

--- index.js
+++ index.js
@@ -16,9 +16,21 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-    // try{
+    try {
+        const portfolioCollection = client.db('portfolio').collection('portfolioData');
 
-    // }
+        app.get('/portfolio', async (req, res) => {
+            const query = {}
+            const cursor = portfolioCollection.find(query)
+            const portfolio = await cursor.toArray();
+            res.send(portfolio);
+        })
+
+    }
+
+    finally {
+
+    }
 
 
 }
